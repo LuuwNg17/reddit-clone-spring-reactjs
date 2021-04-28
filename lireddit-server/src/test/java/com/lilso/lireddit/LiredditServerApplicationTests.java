d72098653077663a2003472abeefb3934e99670e
Nguyễn Thái Dương <nguyenthaiduong@ThaiDuong-Ng.local>
2021-04-29 01:08:03 +0700

    settup pj

---
+++ lireddit-server/src/test/java/com/lilso/lireddit/LiredditServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lilso.lireddit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiredditServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
